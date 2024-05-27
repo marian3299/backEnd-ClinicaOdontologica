be12a742e81b8e44c348963c2b1a2abdc82fb0b7
mariana3299 <mariana.ag3299@gmail.com>
2024-05-27 13:40:29 -0600

    feat: adding getAll and guardarOdont to OdontologoController

--- src/main/java/BackEndHD/ClinicaOdontologica/controller/OdontologoController.java
+++ src/main/java/BackEndHD/ClinicaOdontologica/controller/OdontologoController.java
@@ -4,12 +4,11 @@ import BackEndHD.ClinicaOdontologica.model.Odontologo;
 import BackEndHD.ClinicaOdontologica.service.OdontologoService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
 @RequestMapping("/odontologos")
 public class OdontologoController {
     OdontologoService odontologoService;
@@ -18,11 +17,22 @@ public class OdontologoController {
         odontologoService = new OdontologoService();
     }
 
+    @GetMapping
+    @ResponseBody
+    public List<Odontologo> buscarTodos(){
+        return odontologoService.listarTodos();
+    }
+
     @GetMapping("/{id}")
     public String buscarOdontPorId(Model  model, @PathVariable("id") Integer id){
         Odontologo odontologo = odontologoService.buscarPorId(id);
         model.addAttribute("nombre", odontologo.getNombre());
         model.addAttribute("apellido", odontologo.getApellido());
         return "odontologos";
+    }
+
+    @PostMapping("/guardar")
+    public Odontologo guardarOdontologo(@RequestBody Odontologo odontologo){
+        return odontologoService.guardarOdontologo(odontologo);
     }
 }
--- src/main/java/BackEndHD/ClinicaOdontologica/controller/PacienteController.java
+++ src/main/java/BackEndHD/ClinicaOdontologica/controller/PacienteController.java
@@ -4,12 +4,9 @@ import BackEndHD.ClinicaOdontologica.model.Paciente;
 import BackEndHD.ClinicaOdontologica.service.PacienteService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/pacientes")
 public class PacienteController {
     private PacienteService pacienteService;
@@ -23,6 +20,6 @@ public class PacienteController {
         Paciente paciente = pacienteService.buscarPorEmail(email);
         model.addAttribute("nombre", paciente.getNombre());
         model.addAttribute("apellido", paciente.getApellido());
-        return "index";
+        return "index"; //En postman no nos va a devolver el html porque cambiamos a RestController para poder probar en postman
     }
 }
